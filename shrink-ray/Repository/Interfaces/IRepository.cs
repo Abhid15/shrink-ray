cf016fb7c30d4f62ed29c50c8dd809f522fe6e1c
Abhishek Das <abhishek_das@protonmail.com>
2021-04-18 21:25:02 +0530

    Repository Project with Interfaces and contract implementations

---
+++ shrink-ray/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Model;
+
+namespace Repository.Interfaces
+{
+    public interface IRepository
+    {
+        IEnumerable<ShrinkRayUrlModel> GetCollectionFromDataStore();
+        ShrinkRayUrlModel GetItemFromDataStoreByShortUrl(string shortUrl);
+        ShrinkRayUrlModel GetItemFromDataStoreByLongUrl(string shortUrl);
+
+        ShrinkRayUrlModel SaveItemToDataStore(ShrinkRayUrlModel model);
+    }
+}
---
+++ shrink-ray/Repository/ShrinkRayUrlRepository.cs
@@ -0,0 +1,46 @@
+﻿using Model;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository
+{
+    public class ShortUrlRepository : BaseMongoRepository<ShrinkRayUrlModel>, IRepository
+    {
+
+        public ShortUrlRepository(string mongoDBConnectionString, string dbName, string collectionName) : base(mongoDBConnectionString, dbName, collectionName)
+        {
+
+        }
+        public IEnumerable<ShrinkRayUrlModel> GetCollectionFromDataStore()
+        {
+            return GetList();
+        }
+
+        public ShrinkRayUrlModel GetItemFromDataStoreByShortUrl(string shortUrl)
+        {
+            return GetBy(c => c.ShortURL == shortUrl);
+        }
+
+
+        public ShrinkRayUrlModel GetItemFromDataStoreByLongUrl(string longUrl)
+        {
+            return GetBy(c => c.LongURL == longUrl);
+        }
+
+        public ShrinkRayUrlModel SaveItemToDataStore(ShrinkRayUrlModel model)
+        {
+            try
+            {
+                this.Create(model);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+            return model;
+        }
+    }
+}
